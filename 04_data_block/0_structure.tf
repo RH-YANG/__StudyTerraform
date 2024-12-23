ba07f83b5cbf44fe9b265c35e31bcc546fe6547c
rh <mago1291@naver.com>
2024-12-23 16:52:33 +0900

    How to use Dada block

---
+++ 04_data_block/0_structure.tf
@@ -0,0 +1,48 @@
+#### 데이터 블록
+#    - 테라폼으로 정의되지 않은 외부 리소스 또는 저장된 정보를 테라폼 내에서 참조할 때 사용한다
+
+
+####  1. 구조
+#    - 데이터 소스 유형에서 프로바이더와 리소스 유형을 확인할 수 있다.
+
+#     data "프로바이더_리소스유형" "필요한경우 식별용 이름" {
+#       <인수> = <값>
+#     } 
+
+# 예시
+data "local_file" "abc" {
+  filename = "${path.module}/abc.txt"
+}
+
+
+#### 2. 참조하기
+#    - 리소스 블록과 구별되게 앞에 data가 붙는다
+#    - data.리소스유형.이름.속성
+
+
+#### 3. 활용예 : aws 프로바이더
+data "aws_availability_zones" "available" {
+  state = "available" # 'available' 상태인 가용 영역만 가져옴
+}
+
+# resource "aws_subnet" "primary" {
+#   ...
+#   availability_zone = data.aws_availability_zones.available.names[0]
+#   ...
+# }
+
+
+#### 4. 활용예 : local 프로바이더
+resource "local_file" "abc" {
+  content  = "123!"
+  filename = "${path.module}/abc.txt"
+}
+
+data "local_file" "abc" {
+  filename = local_file.abc.filename
+}
+
+resource "local_file" "def" {
+  content  = data.local_file.abc.content
+  filename = "${path.module}/def.txt"
+}
