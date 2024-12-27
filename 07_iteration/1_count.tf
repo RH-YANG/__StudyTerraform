d10080d695d427688f77b89c736c4ce94eee8cf9
rh <mago1291@naver.com>
2024-12-27 16:09:53 +0900

    Add usage with ternary operator

--- 07_iteration/1_count.tf
+++ 07_iteration/1_count.tf
@@ -12,3 +12,16 @@ resource "local_file" "abc" {
   # filename = "${path.module}/abc.txt" 이렇게 정의하면 덮어써져서 결과적으로는 1개만 생성된다.
   filename = "${path.module}/abc${count.index}.txt"
 }
+
+
+
+# 3항 조건식과 결함하여 리소스를 생성할지 여부를 지시할 수 있다.
+variable "enable_file" {
+  default = true
+}
+
+resource "local_file" "foo" {
+  count    = var.enable_file ? 1 : 0 # 참인 경우에만 리소스가 생성된다.
+  content  = "foo!"
+  filename = "${path.module}/foo.bar"
+}
